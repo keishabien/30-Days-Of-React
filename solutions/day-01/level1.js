2d91d4ce4e16f0b547de10e8d7fca2825b82a186
keishabien <keisha@bmediallc.com>
2024-12-03 16:46:55 -0600

    feat (day 01): round number for middle of array

--- solutions/day-01/level1.js
+++ solutions/day-01/level1.js
@@ -1,15 +1,24 @@
 const newArray = ['One', 'two', "Thr33", "4our", "five"];
 console.log(newArray);
-console.log(newArray.length);
+console.log(Math.ceil(newArray.length / 2));
 
+let middleIndex = newArray.length / 2;
+middleIndex = Math.floor(middleIndex);
 const lastIndex = newArray.length - 1;
-console.log(newArray[0] + ", " + newArray[2]  + ", " + newArray[lastIndex])
+console.log(newArray[0] + ", " + newArray[middleIndex]  + ", " + newArray[lastIndex])
 
 const mixedDataTypes = [
     '30DaysOfReact',
+    '30DaysOfCoolness',
     666,
+    999,
     true,
     { country: 'Merica', city: 'Homieville' },
-    { skills: 'cool', 'funny', 'thinker' }
+    { skills: ['cool', 'funny', 'thinker'] }
 ];
 console.log(mixedDataTypes);
+
+const itCompanies = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"];
+console.log(itCompanies);
+console.log(itCompanies.length / 2);
+
