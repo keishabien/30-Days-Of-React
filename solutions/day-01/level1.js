f180826749e35ef0cfca54369ab7aaac57349ee3
keishabien <keisha@bmediallc.com>
2024-12-03 16:03:33 -0600

    feat (day 01): get first, middle, last item in array

--- solutions/day-01/level1.js
+++ solutions/day-01/level1.js
@@ -1,3 +1,7 @@
 const newArray = ['One', 'two', "Thr33", "4our", "five"];
 
 console.log(newArray);
+console.log(newArray.length);
+
+const lastIndex = newArray.length - 1;
+console.log(newArray[0] + ", " + newArray[2]  + ", " + newArray[lastIndex])
