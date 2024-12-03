a89a2feb1736a373cae8ec7c6851d8a60636f1d9
keishabien <keisha@bmediallc.com>
2024-12-03 15:38:49 -0600

    chore: remove previous pasted code; create empty array with 8 values

--- introduction.js
+++ introduction.js
@@ -1,27 +1,4 @@
 console.log('Welcome to 30DaysOfJavaScript')
 
-const arr = [
-    'Asabeneh',
-    250,
-    true,
-    { country: 'Finland', city: 'Helsinki' },
-    { skills: ['HTML', 'CSS', 'JS', 'React', 'Python'] },
-] // arr containing different data types
-console.log(arr)
-
-const fruits = ['banana', 'orange', 'mango', 'lemon']
-let firstFruit = fruits[0] // we are accessing the first item using its index
-
-console.log(firstFruit) // banana
-
-secondFruit = fruits[1]
-console.log(secondFruit) // orange
-
-let lastFruit = fruits[3]
-console.log(lastFruit) // lemon
-// Last index can be calculated as follows
-
-let lastIndex = fruits.length - 1
-lastFruit = fruits[lastIndex]
-
-console.log(lastFruit) // lemon
+const eightEmptyValues = Array(8) // it creates eight empty values
+console.log(eightEmptyValues) // [empty x 8]
