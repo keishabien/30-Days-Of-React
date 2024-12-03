6c8e480fad7cb041c1b8ac5f35382cc362761aab
keishabien <keisha@bmediallc.com>
2024-12-03 16:06:20 -0600

    bug (day 01): wrong array type defined

--- solutions/day-01/level1.js
+++ solutions/day-01/level1.js
@@ -1,7 +1,15 @@
 const newArray = ['One', 'two', "Thr33", "4our", "five"];
-
 console.log(newArray);
 console.log(newArray.length);
 
 const lastIndex = newArray.length - 1;
 console.log(newArray[0] + ", " + newArray[2]  + ", " + newArray[lastIndex])
+
+const mixedDataTypes = [
+    '30DaysOfReact',
+    666,
+    true,
+    { country: 'Merica', city: 'Homieville' },
+    { skills: 'cool', 'funny', 'thinker' }
+];
+console.log(mixedDataTypes);
